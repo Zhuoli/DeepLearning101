f305b5134045aa38011915f2712650dbc3fcec00
zhuolil <cp8482617>
2017-01-22 23:12:27 -0800

    lung competition

---
+++ kaggle/lung_preprocessing.py
@@ -0,0 +1,16 @@
+# %matplotlib inline
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import dicom
+import os
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+# Some constants 
+INPUT_FOLDER = '../../input'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
